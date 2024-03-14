e503ee4da4aee6002e57a47c53888ba7f0c75d86
Folahan <seunfola1@gmail.com>
2024-03-14 13:19:19 +0100

    all

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,19 +1,26 @@
-import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth";
+import { initializeApp, getApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+let app;
 
+// Check if Firebase app is already initialized
+try {
+  app = getApp();
+} catch (e) {
+  // Firebase app is not initialized, so initialize it
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCDg7IdSUj-iFbZM8RLfW5Uqu8s46MZF1o",
-  authDomain: "taskdashboard-f51ba.firebaseapp.com",
-  projectId: "taskdashboard-f51ba",
-  storageBucket: "taskdashboard-f51ba.appspot.com",
-  messagingSenderId: "719842361150",
-  appId: "1:719842361150:web:e8debaa176148a0bc6f66f",
-  measurementId: "G-J1F3RF3SYT"
-};
+  app = initializeApp(firebaseConfig);
+}
 
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth (app);
-export const googleProvider =  new GoogleAuthProvider();
+// Export Firebase auth and GoogleAuthProvider
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
