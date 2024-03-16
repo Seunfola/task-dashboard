4ba7b330b2e06ede032ff1c7d087fcfe4a1fba0f
Folahan <seunfola1@gmail.com>
2024-03-16 15:33:25 +0100

    navbar responsiveness ongoing

--- src/pages/Navbar/Navbar.js
+++ src/pages/Navbar/Navbar.js
@@ -1,56 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Filter from '../../../components/Filter/index';
 import Search from '../../../components/Search/index';
 import Logout from '../Logout';
-import './navbar.module.css';
+import { BiAlignJustify } from 'react-icons/bi';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 const Navbar = ({ authUser }) => {
-    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-    const toggleMobileMenu = () => {
-        setMobileMenuOpen(!isMobileMenuOpen);
-    };
+    const [menuOpened, setMenuOpened] = useState(false);
+    const [menuStyles, setMenuStyles] = useState({});
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (typeof window !== 'undefined') {
+                const clientWidth = document.documentElement.clientWidth;
+                setMenuStyles(clientWidth <= 780 ? { right: !menuOpened && "-100%" } : {});
+            }
+        };
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [menuOpened]);
 
     return (
         <div className="navbar">
             <div className="nav-bar">
                 <header>Task Dashboard</header>
             </div>
-            <div className="navbar-envelop">
-                {/* Hamburger Icon for Mobile */}
-                <dgiv className={`hamburger-icon ${isMobileMenuOpen ? 'hide' : ''}`} onClick={toggleMobileMenu}>
-                    <div className={`line ${isMobileMenuOpen ? 'open' : ''}`}></div>
-                    <div className={`line ${isMobileMenuOpen ? 'open' : ''}`}></div>
-                    <div className={`line ${isMobileMenuOpen ? 'open' : ''}`}></div>
-                </dgiv>
-
-                {/* Filter and Search */}
-                <div className={`filter-container ${isMobileMenuOpen ? 'show' : ''}`}>
-                    <Filter />
-                </div>
-                <div className={`search-container ${isMobileMenuOpen ? 'show' : ''}`}>
-                    <Search />
-                </div>
-
-                {/* Logout Button */}
-                <div className={`navbar-list ${isMobileMenuOpen ? 'show' : ''}`}>
-                    {authUser ? (
-                        <>
-                            <div key="logout" className="navbar-item">
-                                <Logout />
+            <div className='navbar-envelop'>
+            <OutsideClickHandler onOutsideClick={() => setMenuOpened(false)}>
+                <div className='h-menu' style={menuStyles}>
+                    <div className="filter-container">
+                        <Filter />
+                    </div>
+                    <div className="search-container">
+                        <Search />
+                    </div>
+                    <div className="navbar-list ">
+                        {authUser ? (
+                            <>
+                                <div key="logout" className="navbar-item">
+                                    <Logout />
+                                </div>
+                                <div>
+                                    <p>{`Signed In as ${authUser.email}`}</p>
+                                </div>
+                            </>
+                        ) : (
+                            <div key="signup" className="navbar-item">
+                                <Link href="/signUp">Register</Link>
                             </div>
-                            <div>
-                                <p>{`Signed In as ${authUser.email}`}</p>
-                            </div>
-                        </>
-                    ) : (
-                        <div key="signup" className="navbar-item">
-                            <Link href="/signUp">Register</Link>
-                        </div>
-                    )}
+                        )}
+                    </div>
                 </div>
-            </div>
+                <div className='menu-icon' onClick={() => setMenuOpened((prev) => !prev)}>
+                    <BiAlignJustify size={35} />
+                </div>
+            </OutsideClickHandler>
+        </div>
         </div>
     );
 };
