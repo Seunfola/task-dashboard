510afb8fa473a193e033870a659121832eb8a8c7
Folahan O Mosunmola <seunfola1@gmail.com>
2024-03-17 09:21:45 +0100

    Update Logout.js

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import { auth } from '../../firebase/firebase';
 import { signOut } from 'firebase/auth';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import Swal from 'sweetalert2';
 
 const Logout = () => {
-  const handleSignOut = async () => {
-    try {
-      await signOut(auth);
-      console.log('Logged out');
-    } catch (error) {
-      console.error('Error signing out:', error.message);
-      toast.error('Error signing out. Please try again.');
-    }
-  };
+    const handleSignOut = async () => {
+        // Display confirmation dialog
+        const confirmed = await Swal.fire({
+            title: 'Are you sure?',
+            text: 'You will be logged out.',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, log me out'
+        });
 
-  return (
-    <div className="logout-container">
-      <ToastContainer />
-      <button className="logout-button" onClick={handleSignOut}>
-        Log Out
-      </button>
-    </div>
-  );
+        if (confirmed.isConfirmed) {
+            try {
+                await signOut(auth);
+                console.log('Logged out');
+            } catch (error) {
+                console.error('Error signing out:', error.message);
+                // Handle error
+            }
+        }
+    };
+
+    return (
+        <div className="logout-container">
+            <button className="logout-button" onClick={handleSignOut}>
+                Log Out
+            </button>
+        </div>
+    );
 };
 
 export default Logout;
